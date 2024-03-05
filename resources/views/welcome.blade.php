7b71d7bf07198cbe943faced194935c4822518e4
= <113957634+febbymathelda@users.noreply.github.com>
2024-03-05 23:30:23 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,5 @@
         <title>PWL_POS</title>
     </head>
     <body>
-        <h2>Welcome to pemrograman web lanjut 2024</h2>
+        <h2>Welcome to pemrograman web lanjut 2024 POLINEMA</h2>
 </html>
