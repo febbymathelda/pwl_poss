f4cdf1ffa79b13df01bc2e8d490f189a8768b1ca
= <113957634+febbymathelda@users.noreply.github.com>
2024-03-09 22:31:32 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,24 +11,33 @@ class UserController extends Controller
     public function index()
     {
         
-        $user = UserModel::firstOrCreate(
+        $user = UserModel::create(
             [
-                //'username' => 'manager',
-                //'nama' => 'Manager',
-                //'username' => 'manager22',
-                //'nama' => 'Manager Dua Dua',
-                //'password' => Hash::make('12345'),
-                //'level_id' => 2
-                //'username' => 'manager',
-                //'nama' => 'Manager',
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+                'level_id' => 2,
+            ]);
+
+            $user->username = 'manager12';
+
+            //$user->isDirty(); //true
+            //$user->isDirty('username'); //true
+            //$user->isDirty('nama'); //false
+            //$user->isDirty(['nama', 'username']); //true
+
+            //$user->isClean();
+            //$user->isClean('username');
+            //$user->isClean('nama');
+            //$user->isClean(['nama', 'username']);
+
         $user->save();
 
-        return view('user', ['data' =>  $user]);
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged('username', 'level_id');
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username',])); //true
+
     }
 }
