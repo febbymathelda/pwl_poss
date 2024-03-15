580107c2bfdb251704d73fbcc168af4903a03916
= <113957634+febbymathelda@users.noreply.github.com>
2024-03-15 07:56:01 +0700

    praktikum 1 job 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,23 @@
-<html>
-    <head>
-        <title>PWL_POS</title>
-    </head>
-    <body>
-        <h2>Welcome to pemrograman web lanjut 2024 POLINEMA</h2>
-</html>
+@extends('layouts.app')
+
+{{-- Customzize layout sections --}}
+
+@section('subtitle', 'Dashboard')
+@section('content_header_title', 'Dashboard')
+{{--@section('content_header_subtitle', 'Dashboard')--}}
+
+{{-- content body: main page content --}}
+
+@section('content_body')
+<p>Welcome to this beautiful admin panel.</p>
+@stop
+
+{{-- push extra CSS --}}
+@push(('css'))
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}} 
+@endpush
+{{-- Push extra scripts --}}
+@push('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!");</script>
+@endpush
