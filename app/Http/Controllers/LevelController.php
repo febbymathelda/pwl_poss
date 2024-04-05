bac0caac5c2c9baa81efdae342e8a8c531f8a64f
= <113957634+febbymathelda@users.noreply.github.com>
2024-04-05 20:03:50 +0700

    Tugas jb 7 Level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -2,52 +2,162 @@
 
 namespace App\Http\Controllers;
 
-use App\DataTables\LevelDataTable;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\DataTables\LevelDataTable;
+use App\Http\Requests\StorePostRequestLevel;
+use App\Models\LevelModel;
+use Illuminate\Http\RedirectResponse as HttpRedirectResponse;
 use Illuminate\Support\Facades\Redirect;
-use Monolog\Level;
-use Illuminate\View\view;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Yajra\DataTables\Facades\DataTables;
 
 class LevelController extends Controller
 {
-    public function index(LevelDataTable $dataTable) 
+    //
+    public function index()
     {
-        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-        // return 'insert data baru berhasil';
-        
-        // $row = DB::update('update m_level set level_nama = ? where level_kode =?',['Customer', 'CUS']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: '. $row.'baris';
+        $breadcrumb = (object) [
+            'title' => 'Daftar level',
+            'list' => ['Home', 'Level']
+        ];
 
-        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
-        // return 'Delete data berhasil. Jumlah data yang dihapus: '. $row.'baris';       
-        
-        // $data = DB::select('select * from m_level ');
-        // return view('level', ['data' => $data]);
+        $page = (object) [
+            'title' => 'Daftar level yang terdaftar dalam sistem'
+        ];
 
-        return $dataTable->render('level.index');
+        $level = LevelModel::all();
+
+        $activeMenu = 'level';
+        return view('level.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+
     }
 
-    public function create(): View 
+    public function create()
     {
-        return view('level.create');
+        $breadcrumb = (object)[
+            'title' => 'Tambah Level',
+            'list' => ['Home', 'Level', 'Tambah']
+        ];
+        $page = (object)[
+            'title' => 'Tambah Level Baru'
+        ];
+        $level = LevelModel::all();
+        $activeMenu = 'level';
+        return view('level.create', ['level' => $level, 'breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
     }
 
-    public function store(Request $request): RedirectResponse
+    public function list(Request $request)
     {
-        // LevelModel::create([
-        //     // 'level_id' => $request->levelId,
-        //     'level_kode' => $request->kodeLevel,
-        //     'level_nama' => $request->namaLevel,
-        $validated = $request->validate([
-            //'level_id' => 'required',
-            'level_kode' => 'required',
-            'level_nama' => 'required',
+        $levels = LevelModel::select('level_id', 'level_kode', 'level_nama');
+
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $levels->where('level_id', $request->level_id);
+        }
+
+        return DataTables::of($levels)
+            ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+            ->addColumn('aksi', function ($level) { // menambahkan kolom aksi
+                $btn = '<a href="' . url('/level/' . $level->level_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/level/' . $level->level_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/level/' . $level->level_id) . '">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+            ->make(true);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'level_kode' => 'required|string|min:3|unique:m_level,level_kode',
+            'level_nama' => 'required|string|max:100'
         ]);
-        $validated = $request->safe()->only(['level_kode', 'level_nama']);
-        $validated = $request->safe()->except(['level_kode', 'level_nama']);;
-    
-        return redirect('/level');
+
+        LevelModel::create([
+            'level_kode' => $request->level_kode,
+            'level_nama' => $request->level_nama
+        ]);
+
+        return redirect('/level')->with('success', 'Data level berhasil disimpan');
+    }
+
+    public function show(String $id)
+    {
+        $level = LevelModel::find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Detail Level',
+            'list' => ['Home', 'Level', 'Detail']
+        ];
+
+        $page = (object) [
+            'title' => 'Detail level'
+        ];
+
+        $activeMenu = 'level'; // set menu yang sedang aktif
+        // $level = LevelModel::all();
+        return view('level.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 
+        'level' => $level, 'activeMenu' => $activeMenu]);
+    }
+
+    public function update(Request $request, String $id)
+    {
+        $request->validate([
+            'level_kode' => 'required|string|min:3|unique:m_level,level_kode, ' . $id . ',level_id',
+            'level_nama' => 'required|string|max:100'
+        ]);
+
+        $level = LevelModel::find($id)->update([
+            'level_kode' => $request->level_kode,
+            'level_nama' => $request->level_nama,
+        ]);
+
+        return redirect('/level')->with('success', 'Data user berhasil diubah');
+    }
+
+    public function edit(String $id)
+    {
+        $level = LevelModel::find($id);
+        // $level = LevelModel::all();
+
+        $breadcrumb = (object) [
+            'title' => 'Edit Level',
+            'list' => ['Home', 'Level', 'Edit']
+        ];
+
+        $page = (object) [
+            'title' => 'Edit level'
+        ];
+
+        $activeMenu = 'level'; // set menu yang sedang aktif
+
+        return view('level.edit', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'level' => $level,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
+    public function destroy(String $id)
+    {
+        $check = LevelModel::find($id);
+
+        // Untuk mengecek apakah data user dengan id yang dimaksud ada atau tidak
+        if (!$check) {
+            return redirect('/level')->with('error', 'Data user tidak ditemukan');
+        }
+
+        try {
+            LevelModel::destroy($id); // Hapus data user
+            return redirect('/level')->with('success', 'Data user berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
+            // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
+            return redirect('/level')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\POSController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use Monolog\Level;
+
 
 
 
@@ -13,26 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-//Level
-Route::get('/level', [LevelController::class, 'index']);
-
-//User
-//Route::get('/user', [UserController::class, 'index'])->name('/user');
-
-//Add User
-// Route::get('/user/tambah', [UserController::class, 'create'])->name('/user/create');
-// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
-// Route::post('/user', [UserController::class, 'tambah_simpan']);
-
-//Update User
-// Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
-// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
-
-//Delete User
-// Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
-
-//Kategori
 Route::get('/kategori', [KategoriController::class, 'index'])->name('/kategori');
 
 //Create Kategori
@@ -45,13 +27,6 @@ Route::put('/kategori/update_simpan/{id}', [KategoriController::class, 'update_s
 
 //Delete Kategori
 Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete'])->name('/kategori/delete');
-
-// User Baru js 6
-Route::get('/user', [UserController::class, 'index'])->name('/user');
-Route::get('/user/create', [UserController::class, 'create'])->name('/user/create');
-Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('/user/edit');
-Route::post('/user', [UserController::class, 'store']);
-
 // Level Baru js 6
 Route::get('/level', [LevelController::class, 'index'])->name('/level');
 Route::get('/level/create', [LevelController::class, 'create'])->name('/level/create');
@@ -61,17 +36,28 @@ Route::post('/level', [LevelController::class, 'store']);
 // m_user
 Route::resource('m_user', POSController::class);
 
-//jobsheet 7
-Route::get('/', [WelcomeController::class,'index']);
-// j 7 prakt 3
-Route::group(['prefix' => 'user'], function (){
-    Route::get('/', [UserController::class, 'index']); //menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']); //menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']); //menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']); //menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']); //menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']); //menampilkan halaman form edit
-    Route::put('/{id}', [UserController::class, 'update']); //menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+// J 7 praktikum 3
+Route::get('/', [WelcomeController::class,'index']); 
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
 
+// Jobsheet 7 Tugas 
+Route::get('/level', [LevelController::class, 'index']);
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']); // menampilkan halaman awal user
+    Route::post('/list', [LevelController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [LevelController::class, 'store']); // menyimpan data user baru
+    Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail user
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [LevelController::class, 'update']); // menyimpan perubahan data user
+    Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data user
 });
